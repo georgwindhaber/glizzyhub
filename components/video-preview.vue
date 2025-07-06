d8aec78fe4c3b6e7a23212d656d6371a7b53f5b3
Georg Windhaber <georg.windhaber@gmail.com>
2025-07-06 13:44:27 +0200

    move video preview to component

---
+++ components/video-preview.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+defineProps<{
+  video: {
+    youtubeVideoId: string;
+    thumbnail: string;
+    title: string;
+    duration: string;
+    channelHandle: string;
+    channelThumbnail: string;
+    channelName: string;
+    views: number;
+    publishedAt: string;
+  };
+}>();
+</script>
+
+<template>
+  <li :key="video.youtubeVideoId" class="flex flex-col gap-2 rounded-xl">
+    <nuxt-link
+      class="relative"
+      :href="`https://youtube.com/watch?v=${video.youtubeVideoId}`"
+      target="_blank"
+    >
+      <img
+        :src="video.thumbnail"
+        :alt="video.title"
+        class="rounded-xl w-full"
+      />
+      <div
+        class="bg-gray-900/90 text-white absolute bottom-2 right-2 rounded px-2"
+      >
+        {{ formatDuration(video.duration) }}
+      </div>
+    </nuxt-link>
+
+    <div class="flex gap-2">
+      <div class="w-fit">
+        <nuxt-link
+          class="relative"
+          :href="`https://youtube.com/${video.channelHandle}`"
+          target="_blank"
+        >
+          <img :src="video.channelThumbnail" class="rounded-full size-12" />
+        </nuxt-link>
+      </div>
+
+      <div class="flex flex-col flex-1">
+        <nuxt-link
+          class="font-bold text-lg"
+          :href="`https://youtube.com/watch?v=${video.youtubeVideoId}`"
+          target="_blank"
+        >
+          {{ video.title }}
+        </nuxt-link>
+        <div class="flex flex-col text-gray-700">
+          <div>
+            <nuxt-link
+              class="hover:text-black"
+              :href="`https://youtube.com/${video.channelHandle}`"
+              target="_blank"
+            >
+              {{ video.channelName }}
+            </nuxt-link>
+          </div>
+          <div>
+            {{ formatViews(video.views) }} views -
+            {{ new Date(video.publishedAt).toLocaleString() }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </li>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -7,65 +7,11 @@ const videosData = await useFetch("/api/videos");
     <ul
       class="grid grid-cols-1 @2xl:grid-cols-2 @4xl:grid-cols-3 @6xl:grid-cols-4 gap-5 p-5"
     >
-      <li
+      <video-preview
         v-for="video of videosData.data.value"
         :key="video.youtubeVideoId"
-        class="flex flex-col gap-2 rounded-xl"
-      >
-        <nuxt-link
-          class="relative"
-          :href="`https://youtube.com/watch?v=${video.youtubeVideoId}`"
-          target="_blank"
-        >
-          <img
-            :src="video.thumbnail"
-            :alt="video.title"
-            class="rounded-xl w-full"
-          />
-          <div
-            class="bg-gray-900/90 text-white absolute bottom-2 right-2 rounded px-2"
-          >
-            {{ formatDuration(video.duration) }}
-          </div>
-        </nuxt-link>
-
-        <div class="flex gap-2">
-          <div class="w-fit">
-            <nuxt-link
-              class="relative"
-              :href="`https://youtube.com/${video.channelHandle}`"
-              target="_blank"
-            >
-              <img :src="video.channelThumbnail" class="rounded-full size-12" />
-            </nuxt-link>
-          </div>
-
-          <div class="flex flex-col flex-1">
-            <nuxt-link
-              class="font-bold text-lg"
-              :href="`https://youtube.com/watch?v=${video.youtubeVideoId}`"
-              target="_blank"
-            >
-              {{ video.title }}
-            </nuxt-link>
-            <div class="flex flex-col text-gray-700">
-              <div>
-                <nuxt-link
-                  class="hover:text-black"
-                  :href="`https://youtube.com/${video.channelHandle}`"
-                  target="_blank"
-                >
-                  {{ video.channelName }}
-                </nuxt-link>
-              </div>
-              <div>
-                {{ formatViews(video.views) }} views -
-                {{ new Date(video.publishedAt).toLocaleString() }}
-              </div>
-            </div>
-          </div>
-        </div>
-      </li>
+        :video="video"
+      />
     </ul>
   </main>
 </template>
